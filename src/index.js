f34484650507a593fcc47554b6bf8ba90bd1e7eb
Yuriy Brovko <ysb.ptc@gmail.com>
2022-05-28 10:41:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 
 import debounce from 'lodash.debounce';
+
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
